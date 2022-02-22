0b38a08bdec277746e3d208ffd77b5559aa60df2
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2022-02-23 00:43:09 +0530

    Added swagger

--- src/main/java/com/js/authentication/AuthenticationApplication.java
+++ src/main/java/com/js/authentication/AuthenticationApplication.java
@@ -2,8 +2,10 @@ package com.js.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
